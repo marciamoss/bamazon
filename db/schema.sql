c1fca6553827ab09fe480464e3c40e167f227353
Autumn4eva <45056799+marciamoss@users.noreply.github.com>
2019-03-06 17:20:14 -0500

    increased length for numbers

--- db/schema.sql
+++ db/schema.sql
@@ -7,9 +7,9 @@ create table products (
  item_id int not null auto_increment,
  product_name varchar(50),
  department_name varchar(50),
- price decimal(10,2),
+ price decimal(20,2),
  stock_quantity int,
- product_sales decimal(10,2),
+ product_sales decimal(20,2),
  primary key(item_id)
 );
 
@@ -18,7 +18,7 @@ select * from products;
 create table departments (
  department_id int not null auto_increment,
  department_name varchar(50),
- over_head_costs decimal(10,2),
+ over_head_costs decimal(20,2),
  primary key(department_id)
 );
 
